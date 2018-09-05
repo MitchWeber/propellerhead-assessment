86c91efba7b71db13fa4913c165c7bcd7cd478ba
MitchWeber <michael.weber@nefkom.net>
2018-09-05 21:01:52 +0200

    Initial commit

---
+++ propellerhead-assessment-backend/src/main/java/com/github/mitchweber/PropellerheadTechnichalAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.github.mitchweber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropellerheadTechnichalAssessmentApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PropellerheadTechnichalAssessmentApplication.class, args);
+	}
+}
